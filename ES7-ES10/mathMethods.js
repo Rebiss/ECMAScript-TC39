7f306557f9b8314bf0bde36f60fb11354f0ba99b
Andranik Barseghyan <andranik.barseghyan@betconstruct.com>
2020-01-04 22:16:13 +0400

    new syntax suger Math, trailing commas

---
+++ ES7-ES10/mathMethods.js
@@ -0,0 +1,26 @@
+Math.pow(7,2); //49
+
+//Exponentiation operator
+const exp = 7**2; // 49
+const ext = 5**3; // 125
+
+Math.pow(7,2) + Math.pow(6,3) / Math.pow(7,2)
+
+(7**2) + (6**3) / 7**2;
+
+/**
+ * !Trailing commas
+ */
+
+ const userInfo = {
+     name: 'Serobik',
+     lastname: 'Blbulyan'
+ }
+ 
+ //new 
+
+ const userInfoNew = {
+    name: 'Serobik',
+    lastname: 'Blbulyan', // add , git 
+    age: '69'
+}
