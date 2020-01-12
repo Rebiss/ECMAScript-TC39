cd52fcc199202e077e9d7945459e161bcd7ad94c
Rebiss <andranik.barseghyan@betconstruct.com>
2020-01-12 21:08:10 +0400

    Add: Random Number Algoritm

--- RAND/random.js
+++ RAND/random.js
@@ -1,7 +1,7 @@
-//Random Method
-
+//Random Method Exemple 1.
 const min = 1;
 const max = 4;
+const experiment = Math.floor(Math.random() * (max - min + 1)) + min; 
 
 const randomGenerator = (min, max) => { Math.floor(Math.random() * (max - min) + min) };
-console.log(randomGenerator(min,max))
+console.log(randomGenerator(min,max));
